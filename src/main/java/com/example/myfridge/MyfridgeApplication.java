ef82f16f771168c46848701d3dc7c16714155fae
pear27 <jinalks43@gmail.com>
2026-02-09 11:16:06 +0900

    first commit

---
+++ src/main/java/com/example/myfridge/MyfridgeApplication.java
@@ -0,0 +1,17 @@
+package com.example.myfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class MyfridgeApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		SpringApplication.run(MyfridgeApplication.class, args);
+	}
+
+}
